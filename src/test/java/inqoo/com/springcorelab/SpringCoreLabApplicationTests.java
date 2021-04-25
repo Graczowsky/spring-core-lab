e691f6cb4d0ae04fdeeaeb35a7a8af3c4306c999
Your Name <you@example.com>
2021-04-25 09:14:19 +0200

    first commit

---
+++ src/test/java/inqoo/com/springcorelab/SpringCoreLabApplicationTests.java
@@ -0,0 +1,13 @@
+package inqoo.com.springcorelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
